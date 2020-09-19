f66b82dce1723d78d8623b933d8616753dd515fc
liu149 <liuqi1212@outlook.com>
2020-09-19 22:42:00 +0800

    模拟springboot

--- src/main/java/com/lq/MySpringBoot.java
+++ src/main/java/com/lq/MySpringBoot.java
@@ -3,6 +3,7 @@ package com.lq;
 import org.apache.catalina.Context;
 import org.apache.catalina.WebResourceRoot;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
 import org.apache.catalina.webresources.StandardRoot;
 
 import java.io.File;
@@ -18,7 +19,11 @@ public class MySpringBoot {
 
 		//这相当于在webapps下添加一个项目，其实一个项目也就是一个目录,路径可以是任意
 		Context context = tomcat.addWebapp("/",new File("src/main/webapp").getAbsolutePath());
-		WebResourceRoot webResource = new StandardRoot(context);
+		WebResourceRoot resources = new StandardRoot(context);
+		resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes",
+				path, "/"));
+		context.setResources(resources);
+
 		tomcat.start();
 		tomcat.getServer().await();
 	}
